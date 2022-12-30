302101f54a5d8648b4196b6f2718885900960801
James Johnson <james@localhost.localdomain>
2022-12-30 08:39:33 -0800

    new file:   inttest.cpp 	new file:   using.cpp

---
+++ prog1/using.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cin;
+using std::cout; using std::endl;
+// showing you can use multiple "using" statements
+int main() {
+	cout << "2 nums:" << endl;
+	int v1, v2;
+	cin >> v1 >> v2;
+	cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
+	return 0;
+}
